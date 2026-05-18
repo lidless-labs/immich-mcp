621b013d74c0a31d4b8992d8d25e8bdf0c1171d9
solomonneas <srneas@gmail.com>
2026-05-17 23:44:24 -0400

    chore: project scaffolding

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node20",
+  outDir: "dist",
+  clean: true,
+  banner: { js: "#!/usr/bin/env node" },
+  sourcemap: false,
+  splitting: false,
+  minify: false,
+});
